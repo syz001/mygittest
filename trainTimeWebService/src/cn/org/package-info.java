24dfd08e7751e19c2293782d558a16f295dfd960
sangyz <hero_sang@126.com>
2019-08-06 10:23:32 +0800

    1111

---
+++ trainTimeWebService/src/cn/org/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <a href="http://www.webxml.com.cn/" target="_blank">WebXml.com.cn</a> <strong>火车时刻表 WEB 服务</strong>提供：站站查询；车次查询；车站所有车次查询。本火车时刻表 WEB 服务提供的列车时刻表数据仅供参考，如有异议以当地铁路部门颁布为准。</br>此火车时刻表Web Services请不要用于任何商业目的，若有需要请<a href="http://www.webxml.com.cn/zh_cn/contact_us.aspx" target="_blank">联系我们</a>，欢迎技术交流。 QQ：8409035<br /><strong>使用本站 WEB 服务请注明或链接本站：http://www.webxml.com.cn/ 感谢大家的支持</strong>！<br /><br />&nbsp;
+ * 
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://WebXml.com.cn/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package cn.org;
