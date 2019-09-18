444dca7fcdae7057896051990ad626aed4b85bc3
sangyz <hero_sang@126.com>
2019-09-18 15:12:01 +0800

    11111

--- gittest/src/gittest/TestJava.java
+++
@@ -1,8 +0,0 @@
-package gittest;
-
-public class TestJava {
-
-	public static void main(String[] args) {
-		System.out.println(111);
-	}
-}
--- testGit/src/testGit/T1.java
+++
@@ -1,9 +0,0 @@
-package testGit;
-
-public class T1 {
-
-	public static void main(String[] args) {
-		//22222 -11111
-		System.out.println(11111);
-	}
-}
